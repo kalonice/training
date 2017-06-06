41ad9d7d7ce24cb5b52ea15f75770add9a9472f1
zingorn <zingorn@gmail.com>
2017-06-06 20:33:27 +0300

    fixing coding style

--- include/example.h
+++ include/example.h
@@ -1,8 +1,8 @@
-#ifndef INCLUDE_CHAPTER_10_H_
-#define INCLUDE_CHAPTER_10_H_
+#ifndef INCLUDE_EXAMPLE_H_
+#define INCLUDE_EXAMPLE_H_
 
 int add(int x, int y);
 
 int mul(int x, int y);
 
-#endif  // INCLUDE_CHAPTER_10_H_
+#endif  // INCLUDE_EXAMPLE_H_
