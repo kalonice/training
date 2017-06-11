12b753aa2bcb941da2b4290d9f3867b223bb0ba8
alexander.kovalenko <alexander.kovalenko@vertoanalytics.com>
2017-06-11 20:10:15 +0300

    least squares and gaussian elimination - first realization

---
+++ include/least_squares.h
@@ -0,0 +1,54 @@
+#ifndef INCLUDE_LEAST_SQUARES_H_
+#define INCLUDE_LEAST_SQUARES_H_
+
+#include <vector>
+#include <functional>
+
+template <typename DataType>
+int least_squares(const std::vector<std::vector<DataType>>& paramethers_matrix,
+                  const std::vector<DataType>& answers,
+                  std::vector<DataType>& result,
+                  std::function<int(std::vector<std::vector<DataType>>&, std::vector<DataType>&)> sole_solver) {
+    /**
+     * paramethers_matrix - матрица восстанавливаемых параметров. т.е. снаружи подсчитаны не производные,
+     *      а коэффициэнты при параметрах в зависимости от значений Xi
+     *      строки здесь - параметры функции, столбцы - зависимость от Xi
+     *      в рещультате у нас некая матрица A
+     * answers - одна строка из Yi
+     */
+
+    /**
+     * приводим "параметры" и "ответы" к системе линейных уравнений и решаем эту систему
+     * если исходные "параметры" - матрица A, а "ответы" - вектор "y",
+     * то система имеет вид :
+     * A*T(A) = y*T(A)
+     * где T(A) - транспонированная матрица A - мы не будем её отдельно хранить,
+     * а будем обращаться к A, хитря с индексами
+     */
+    std::size_t rows = paramethers_matrix.size();
+    std::size_t cols = paramethers_matrix[0].size();
+    // sole = System of linear equations = СЛАУ = Система Линейных Алгебраических Уравнений
+    std::vector<std::vector<DataType>> sole_matrix(rows, std::vector<DataType>(rows));
+    std::vector<DataType> sole_constant_terms(rows);
+    for (std::size_t i = 0; i < rows; ++i) {
+        for (std::size_t j = 0; j < rows; ++j) {
+            for (std::size_t k = 0; k < cols; ++k) {
+                sole_matrix[i][j] += paramethers_matrix[j][k] * paramethers_matrix[i][k];
+            }
+        }
+        for (std::size_t k = 0; k < cols; ++k) {
+            sole_constant_terms[i] += answers[k] * paramethers_matrix[i][k];
+        }
+    }
+
+    int result_code = sole_solver(sole_matrix, sole_constant_terms);
+    if (result_code == 0) {
+        result = sole_constant_terms;
+    }
+
+    return result_code;
+}
+
+
+
+#endif  // INCLUDE_LEAST_SQUARES_H_
