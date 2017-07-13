14c4350157793a2b6a2b46b1f60c38893042c5c2
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-07-14 01:53:04 +0300

    Finish comments & readme.md

--- include/calculator.h
+++ include/calculator.h
@@ -11,13 +11,13 @@
 
 namespace mycalc {
 /**
- *
+ * Класс для обработки метематического выражения с "длинными" числами
  */
 class Calculator {
  public:
   /**
    * Конструктор
-   * @param input_expression входное выражение
+   * @param input_expression входное математическое выражение с "длинными" числами
    */
   explicit Calculator(const std::string& input_expression);
   /**
@@ -25,8 +25,9 @@ class Calculator {
    */
   ~Calculator() {}
   /**
-   * Вычисленеие выражения и запись результата
-   * @return указатель на BigInteger
+   *
+   * @return Результат вычисления выражения. В случае наличия недопустимого выражения или переполнения в ходе
+   * обработки возвращает nullptr, в противном случае - указатель на "длинное" число типа BigInteger
    */
   std::unique_ptr<BigInteger> CalcExpression();  // вычисленеие выражения и запись результата
 
@@ -34,7 +35,7 @@ class Calculator {
   void ParseExpression();  // создание обратной польской записи и анализ допустимости выражения
   std::unique_ptr<BigInteger> Calculate();  // получение результата из сформированной обратной польской записи
   void ProcessOperationStack(std::unique_ptr<IOperation> cur_operation);  // Обработка стека
-  void moveOperationToRpn();
+  void moveOperationToRpn();  // функция обработки операций и помещения ее в обратную польскую запись
 
   std::string expression;  // входное выражение
   bool expression_is_valid;  // флаг валидности выражения
--- include/mathoperations.h
+++ include/mathoperations.h
@@ -12,7 +12,7 @@ namespace mycalc {
 /**
  * приоритет для выполнения математических операций.
  * OPEN_PARENTHESIS и CLOSE_PARENTHESIS имеют различные приоритеты
- * для возможности их различения, т.к. метод getType() не предусмотрен
+ * для возможности их различения
  */
 
 enum OperationPriority {
@@ -24,10 +24,7 @@ enum OperationPriority {
 };
 
 /**
- * Интерфейс базового класса для математических операций.
- * - Метод create() - фабрика создания математичекой операции, заданной
- * соответствующим символом.
- * - Метод getPriority() возвращает приоритет соответствующей операции
+ * Интерфейс базового класса для математических операций
  */
 
 class IOperation : public IBaseElement {
@@ -38,43 +35,115 @@ class IOperation : public IBaseElement {
 };
 
 /**
- * Классы математических операций.
- * Метод apply() применяет соответствующую операция к двум верхним элементам
- * в стеке при расчете обратной польской последовательности
+ * Класс для математической операции '+'
  */
-
 class OperationPlus : public IOperation {
  public:
+  /**
+   *
+   * @return Возвращает приоритет математической операции - PLUS типа OperationPriority
+   */
   OperationPriority getPriority() const override;
+  /**
+   *
+   * @param rpn_stack Стек с числами над которыми выполняется операция '+',
+   * результат выполнения операции помещается наверх стека
+   * @return Возвращает всегда true
+   */
   bool apply(std::stack<BigInteger*>* rpn_stack) override;
+  /**
+   * Деструктор
+   */
   ~OperationPlus() {}
 };
 
+/**
+ * Класс для математической операции '-'
+ */
 class OperationMinus : public IOperation {
  public:
+  /**
+   *
+   * @return Возвращает приоритет математической операции - MINUS типа OperationPriority
+   */
   OperationPriority getPriority() const override;
+  /**
+   *
+   * @param rpn_stack Стек с двумя числами над которыми выполняется операция '-',
+   * результат выполнения операции помещается наверх стека
+   * @return Возвращает всегда true
+   */
   bool apply(std::stack<BigInteger*>* rpn_stack) override;
+  /**
+   * Деструктор
+   */
   ~OperationMinus() {}
 };
 
+/**
+ * Класс для математической операции '*'
+ */
 class OperationMultiple : public IOperation {
  public:
+  /**
+   *
+   * @return Возвращает приоритет математической операции - MULTIPLY типа OperationPriority
+   */
   OperationPriority getPriority() const override;
+  /**
+   *
+   * @param rpn_stack Стек с двумя числами над которыми выполняется операция '*',
+   * результат выполнения операции помещается наверх стека
+   * @return Возвращает всегда true
+   */
   bool apply(std::stack<BigInteger*>* rpn_stack) override;
+  /**
+   * Деструктор
+   */
   ~OperationMultiple() {}
 };
 
+/**
+ * Класс для математической операции '('
+ */
 class OperationParenthOpen : public IOperation {
  public:
+  /**
+   *
+   * @return Возвращает приоритет математической операции - OPEN_PARENTHESIS типа OperationPriority
+   */
   OperationPriority getPriority() const override;
+  /**
+   *
+   * @param rpn_stack Стек с двумя числами над которыми выполняется операция
+   * @return Возвращает всегда true
+   */
   bool apply(std::stack<BigInteger*>* rpn_stack) override;
+  /**
+   * Деструктор
+   */
   ~OperationParenthOpen() {}
 };
 
+/**
+ * Класс для математической операции '('
+ */
 class OperationParenthClose : public IOperation {
  public:
+  /**
+   *
+   * @return Возвращает приоритет математической операции - CLOSE_PARENTHESIS типа OperationPriority
+   */
   OperationPriority getPriority() const override;
+  /**
+   *
+   * @param rpn_stack Стек с двумя числами над которыми выполняется операция
+   * @return Возвращает всегда true
+   */
   bool apply(std::stack<BigInteger*>* rpn_stack) override;
+  /**
+   * Деструктор
+   */
   ~OperationParenthClose() {}
 };
 
